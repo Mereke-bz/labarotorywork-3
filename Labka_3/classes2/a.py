6aa92ac75ff971575e7e8bdbd85ba4bc03509148
Mereke-bz <mereke.bazarbaev842@gmail.com>
2025-02-03 23:17:41 +0500

    Finish

--- Labka_3/classes2/a.py
+++ Labka_3/classes2/a.py
@@ -1,16 +1,14 @@
-# Определяем класс
+
 class StringProcessor:
-    # Метод для получения строки от пользователя
     def getString(self):
-        self.text = input("Введите строку: ")  # Сохраняем строку в атрибут text
+        self.text = input("Введите строку: ")  
 
-    # Метод для вывода строки в верхнем регистре
+    
     def printString(self):
-        print(self.text.upper())  # Выводим строку в верхнем регистре
+        print(self.text.upper())  
 
-# Создание объекта класса
 sp = StringProcessor()
 
-# Вызов методов
-sp.getString()    # Пользователь вводит строку
-sp.printString()  # Вывод этой строки в верхнем регистре
+
+sp.getString()    
+sp.printString()  
